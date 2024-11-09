2891f55fdb6fe9182c196412ac4f47e5a53788a3
Paramee Saejia <paramee.sae@ku.th>
2024-11-09 13:56:19 +0700

    Describe the changes made in submodule

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 - Fork, then, clone this repo
 - Read the instructions given in the course's Google Classroom and start coding
 - Once you are done, push your final code to your Github repo and modify this README to report on the work you have done
+
+How to use the code
+1. you must crate object from Run class and take argument of class is choice that you want to run
+ex. i want to run choice 1
+i write code:
+choice = Run(1)
+
+Bug of code:
+this code cant  to run choice 9 when running it will occur black Turtle screen and nothing happen
--- polygon_art.py
+++ polygon_art.py
@@ -1,52 +1,120 @@
 import turtle
 import random
+class Polygon:
+    def __init__(self, num_sides ):
+        self.num_sides = num_sides
+        self.size = random.randint(50, 150)
+        self.orientation = random.randint(0, 90)
+        self.location = [random.randint(-250, 250), random.randint(-100, 100)]
+        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        self.border_size = random.randint(1, 10)
+        self.t= turtle.Turtle()
+        self.t.pensize(random.uniform(0.05, 10))
+        self.t.speed(0)
+        self.t.hideturtle()
 
-def draw_polygon(num_sides, size, orientation, location, color, border_size):
-    turtle.penup()
-    turtle.goto(location[0], location[1])
-    turtle.setheading(orientation)
-    turtle.color(color)
-    turtle.pensize(border_size)
-    turtle.pendown()
-    for _ in range(num_sides):
-        turtle.forward(size)
-        turtle.left(360/num_sides)
-    turtle.penup()
 
-def get_new_color():
-    return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    def draw_polygon(self):
 
-turtle.speed(0)
-turtle.bgcolor('black')
-turtle.tracer(0)
-turtle.colormode(255)
 
-# draw a polygon at a random location, orientation, color, and border line thickness
-num_sides = random.randint(3, 5) # triangle, square, or pentagon
-size = random.randint(50, 150)
-orientation = random.randint(0, 90)
-location = [random.randint(-300, 300), random.randint(-200, 200)]
-color = get_new_color()
-border_size = random.randint(1, 10)
-draw_polygon(num_sides, size, orientation, location, color, border_size)
+        #set angle
+        self.t.penup()
+        self.t.goto(self.location[0], self.location[1])
+        self.t.setheading(self.orientation)
+        self.t.pencolor(self.color)
+        self.t.pensize(self.border_size)
+        self.t.pendown()
+        angle = 360 / self.num_sides
+        for _ in range(self.num_sides):
+            self.t.forward(self.size)
+            self.t.left(angle)
+        turtle.penup()
 
-# specify a reduction ratio to draw a smaller polygon inside the one above
-reduction_ratio = 0.618
 
-# reposition the turtle and get a new location
-turtle.penup()
-turtle.forward(size*(1-reduction_ratio)/2)
-turtle.left(90)
-turtle.forward(size*(1-reduction_ratio)/2)
-turtle.right(90)
-location[0] = turtle.pos()[0]
-location[1] = turtle.pos()[1]
+class  EmbeddedPolygon(Polygon):
+    def __init__(self,num_sides):
+        super().__init__(num_sides)
+        self.reduction_ratio = 0.618
 
-# adjust the size according to the reduction ratio
-size *= reduction_ratio
 
-# draw the second polygon embedded inside the original 
-draw_polygon(num_sides, size, orientation, location, color, border_size)
 
-# hold the window; close it by clicking the window close 'x' mark
-turtle.done()
+    def draw_embedpolygon(self ):
+        for _ in range(self.num_sides):
+            self.draw_polygon()
+            self.__reposition()
+
+
+
+
+    def __reposition(self):
+        self.size *= self.reduction_ratio
+
+        self.t.penup()
+        self.t.forward(self.size * (1 - self.reduction_ratio) / 2)
+        self.t.left(90)
+        self.t.forward(self.size * (1 - self.reduction_ratio) / 2)
+        self.t.right(90)
+        self.location[0] = self.t.pos()[0]
+        self.location[1] = self.t.pos()[1]
+
+class Run():
+    def __init__(self, choice):
+
+        screen = turtle.Screen()
+        screen.bgcolor('black')
+        screen.setup(800, 600)
+        screen.colormode(255)
+        if choice == 1 or choice == 2 or choice == 3 or choice == 4:
+            n_poly = 20
+            if choice == 1:
+               for i in range(n_poly): self.__normal_poly(3)
+            if choice == 2:
+                for i in range(n_poly): self.__normal_poly(4)
+            if choice == 3:
+                for i in range(n_poly): self.__normal_poly(5)
+            if choice == 4:
+                action = self.__normal_poly
+                for i in range(n_poly):
+                    action(random.choice([3, 4, 5]))
+
+        if choice == 5 or choice == 6 or choice == 7 or choice == 8:
+             n_poly = 20
+             if choice == 5:
+                for i in range(n_poly): self.__embedded_poly(3)
+             if choice == 6:
+                for i in range(n_poly): self.__embedded_poly(4)
+             if choice == 7:
+                 for i in range(n_poly): self.__embedded_poly(5)
+             if choice == 8:
+                 action = self.__embedded_poly
+                 for i in range(n_poly):
+                    action(random.choice([3, 4, 5]))
+
+             if choice == 9:
+                 action = [
+                     self.__normal_poly, self.__embedded_poly
+                 ]
+                 for i in range(n_poly):
+                    random.choice(action)(random.choice([3, 4, 5]))
+
+
+
+
+
+
+
+    def __normal_poly(self,num_sides):
+        poly = Polygon(num_sides)
+        poly.draw_polygon()
+
+    def __embedded_poly(self,num_sides):
+        poly = EmbeddedPolygon(num_sides)
+        poly.draw_embedpolygon()
+
+
+
+choice = Run(choice=1)
+
+
+
+
